e04c12b4ee77f3027a497415e41879d7d927c717
TobiOladoja <tbfullstack@gmail.com>
2020-01-21 13:30:39 +0000

    Added Styling

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,16 +3,36 @@ import React from 'react';
 const Weather = props => {
   const { temperature, city, country, humidity, description, error } = props;
   return (
-    <div>
+    <div className='weather__info '>
       {city && country && (
-        <p>
-          Location: {city}, {country}{' '}
+        <p className='weather__key'>
+          {' '}
+          Location:
+          <span className='weather__value'>
+            {city}, {country}{' '}
+          </span>
         </p>
       )}
-      {temperature && <p> Temperature: {temperature} </p>}
-      {humidity && <p> Humidity: {humidity} </p>}
-      {description && <p> Conditions: {description}</p>}
-      {error && <p>{error}</p>}
+      {temperature && (
+        <p className='weather__key'>
+          Temperature:
+          <span className='weather__value'> {temperature} </span>
+        </p>
+      )}
+      {humidity && (
+        <p className='weather__key'>
+          Humidity:
+          <span className='weather__value'> {humidity}</span>{' '}
+        </p>
+      )}
+      {description && (
+        <p className='weather__key'>
+          {' '}
+          Conditions:
+          <span className='weather__value'> {description}</span>
+        </p>
+      )}
+      {error && <p className='weather__error'>{error}</p>}
     </div>
   );
 };
